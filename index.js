b43db7bcf1cf9555d381ba26054f748005424272
Damian <themrdamus@gmail.com>
2018-05-02 23:05:44 +0100

    update

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-var mapimg;
+var mapImg;
 
-var clat = 0;
-var clon = 0;
+var centerLatitude = 0;
+var centerLongitude = 0;
 
-var lat = 49.2827;
-var lon = -123.1207;
+var latitude = 49.2827;
+var longitude = -123.1207;
 
 var zoom = 1;
 var earthquakes;
 
 function preload() {
-  mapimg = loadImage("https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,1/1024x512?access_token=pk.eyJ1IjoibXJkYW11cyIsImEiOiJjamdwMTFpbXAwZmF4MzNvMnJueWhvazVkIn0.HaAUa8rcmLQkH69ICwqPDg");
-  earthquakes = loadStrings('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv');
+  mapImg = loadImage("https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,1/1024x512?access_token=pk.eyJ1IjoibXJkYW11cyIsImEiOiJjamdwMTFpbXAwZmF4MzNvMnJueWhvazVkIn0.HaAUa8rcmLQkH69ICwqPDg");
+  earthquakes = loadStrings('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.csv');
 }
 
 function setup () {
   createCanvas(1024, 512);
   translate(width / 2, height / 2);
   imageMode(CENTER);
-  image(mapimg, 0, 0);
+  image(mapImg, 0, 0);
 
-  var cx = mercX(clon);
-  var cy = mercY(clat);
+  var cx = mercX(centerLongitude);
+  var cy = mercY(centerLatitude);
 
-  var x = mercX(lon) - cx;
-  var y = mercY(lat) - cy;
+  // var x = mercX(longitude) - cx;
+  // var y = mercY(latitude) - cy;
 
 
 
   for (var i = 0; i < earthquakes.length; i++) {
     var data = earthquakes[i].split(/,/);
     //console.log(data);
-    var lat = data[1];
-    var lon = data[2];
-    var x = mercX(lon) - cx;
-    var y = mercY(lat) - cy;
+    var latitude = data[1];
+    var longitude = data[2];
+    var x = mercX(longitude) - cx;
+    var y = mercY(latitude) - cy;
 
     var mag = data[4];
     mag = pow(10, mag);
@@ -46,6 +46,7 @@ function setup () {
     var d = map(mag, 0, magMax, 0, circleSize);
 
     fill(255, 0, 255, 200);
+    stroke(100, 100);
     ellipse(x, y, d , d);
   }
 
@@ -59,9 +60,9 @@ function mercX(lon) {
 }
 
 function mercY(lat) {
-  lat = radians(lat);
+  latitude = radians(lat);
   var a = (256 / PI) * pow(2, zoom);
-  var b = tan(PI / 4 + lat / 2);
+  var b = tan(PI / 4 + latitude / 2);
   var c = PI - log(b);
   return a * c;
 }
